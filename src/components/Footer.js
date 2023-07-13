49dd7cdd6202348add3e63e727ebd88515995176
eric <erictranchell@yahoo.com>
2023-07-13 15:54:09 -0400

    footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a
+          href='https://github.com/etranchell'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          GitHub
+          <img
+            src='https://img.icons8.com/fluent/48/000000/github.png'
+            alt='GitHub'
+          />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/eric-tranchell-46b5a0283/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          LinkedIn
+          <img
+            src='https://img.icons8.com/fluent/48/000000/linkedin.png'
+            alt='LinkedIn'
+          />
+        </a>
+        <a
+          href='https://stackoverflow.com/users/21230778/etranchell'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Stack Overflow
+          <img
+            src='https://img.icons8.com/color/48/000000/stackoverflow.png'
+            alt='Stack Overflow'
+          />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
