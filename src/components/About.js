68f7a35a9540249f7fe1e11cc63ec4398c852187
eric <erictranchell@yahoo.com>
2023-07-18 00:52:51 -0400

    picture fix

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,10 @@ const About = () => {
     <section id='About'>
       <h2>About Me</h2>
       <div>
-        {/* <img src='professional pic' */}
-        Eric Tranchell
+        <img
+          src='https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=Brown&facialHairType=BeardMajestic&facialHairColor=Brown&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Light'
+          alt='Eric Tranchell'
+        />
         <p>Hi im Eric</p>
       </div>
     </section>
