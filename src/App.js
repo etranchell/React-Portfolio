85157cfb05a8eae0a370ded190066c70fd24b5bb
eric <erictranchell@yahoo.com>
2023-07-17 21:10:40 -0400

    fix, added files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import About from './components/About';
-import Portfolio from './components/Portfolio';
+import Portfolio from './components/Project';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id='About'>
+      <h2>About Me</h2>
+      <div>
+        <img src='professional pic' alt='Eric Tranchell' />
+        <p>Hi im Eric</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
