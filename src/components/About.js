3262baba5b60380fc313be63d9bf1fd6cd207f0a
eric <erictranchell@yahoo.com>
2023-07-18 01:10:12 -0400

    about me

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
           src='https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=Brown&facialHairType=BeardMajestic&facialHairColor=Brown&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Light'
           alt='Eric Tranchell'
         />
-        <p>Hi im Eric</p>
+        <p>Hi I'm Eric. I'm 38 and live in Traverse City.  I'm about to finish MSU's Full Stack Bootcamp.  I am looking forward to joining the tech work world soon!</p>
       </div>
     </section>
   );
